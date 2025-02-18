c8b4f40e5b8c0d4b1e68e66ffe4f658d393b2e58
Enzo Giuseppin <engiusep@z4r9p5.42lyon.fr>
2025-02-18 14:01:05 +0100

    oui

--- so_long/flood_fill.c
+++ so_long/flood_fill.c
@@ -6,38 +6,37 @@
 /*   By: engiusep <engiusep@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 15:18:34 by engiusep          #+#    #+#             */
-/*   Updated: 2025/02/06 11:58:56 by engiusep         ###   ########.fr       */
+/*   Updated: 2025/02/18 14:00:20 by engiusep         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	flood_fill(t_map *map,char **grid, int y, int x)
+int	flood_fill(t_map *map, char **grid, int y, int x)
 {
-	if(x < 0 || x >= map->width || y < 0 || y >= map->height)
-		return -1;
-		
-	if(grid[y][x] == 'F' || (grid[y][x] != 'P' && grid[y][x] != '0' && grid[y][x] != 'C' && grid[y][x] != 'E'))
-		return 1;
-		
+	if (x < 0 || x >= map->width || y < 0 || y >= map->height)
+		return (-1);
+	if (grid[y][x] == 'F' || (grid[y][x] != 'P' && grid[y][x] != '0'
+			&& grid[y][x] != 'C' && grid[y][x] != 'E'))
+		return (1);
 	grid[y][x] = 'F';
-	flood_fill(map,grid,y + 1, x);
-	flood_fill(map,grid,y, x + 1);
-	flood_fill(map,grid,y - 1, x);
-	flood_fill(map,grid,y,x - 1);
-	
-	return 0;
+	flood_fill(map, grid, y + 1, x);
+	flood_fill(map, grid, y, x + 1);
+	flood_fill(map, grid, y - 1, x);
+	flood_fill(map, grid, y, x - 1);
+	return (0);
 }
-char *cpy_string(t_map *map,char *str)
+char	*cpy_string(t_map *map, char *str)
 {
-	int i;
-	char *cpy;
+	int		i;
+	char	*cpy;
+
 	i = 0;
 	(void)map;
 	cpy = malloc(ft_strlen(str) + 1);
-	if(!cpy)
+	if (!cpy)
 		return (NULL);
-	while(str[i])
+	while (str[i])
 	{
 		cpy[i] = str[i];
 		i++;
@@ -45,66 +44,65 @@ char *cpy_string(t_map *map,char *str)
 	cpy[i] = '\0';
 	return (cpy);
 }
-char **cpy(t_map *map)
+char	**cpy(t_map *map)
 {
-	int i;
-	int j;
-	char **cpy;
+	int		i;
+	int		j;
+	char	**cpy;
+
 	i = 0;
 	j = 0;
 	cpy = malloc((map->height + 1) * sizeof(char *));
-	if(!cpy)
+	if (!cpy)
 		return (NULL);
-	while(i < map->height)
+	while (i < map->height)
 	{
-		cpy[i] = cpy_string(map,map->grid[i]);
+		cpy[i] = cpy_string(map, map->grid[i]);
 		i++;
 	}
 	cpy[i] = NULL;
 	return (cpy);
 }
-int check_collectible_flood(t_map *map,char **grid,t_data *data)
+int	check_collectible_flood(t_map *map, char **grid, t_data *data)
 {
-    int i;
-    int j;
-	
-    i = 0;
-    while(i < map->height)
-    {
-        j = 0;
-        while(j < map->width)
-        {
-            if(grid[i][j] == 'F' || grid[i][j] == '1')
-            	j++;
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < map->height)
+	{
+		j = 0;
+		while (j < map->width)
+		{
+			if (grid[i][j] == 'F' || grid[i][j] == '1')
+				j++;
 			else
 			{
-				write(2,"ERRORR MAP\n",12);
-				free_cpy(grid,map->height);
+				write(2, "ERRORR MAP\n", 12);
+				free_cpy(grid, map->height);
 				exit_prog(data);
 				exit(0);
 			}
-        }
-        i++;
-    }
-    return (0);
+		}
+		i++;
+	}
+	return (0);
 }
-int check_flood(t_map *map,t_data *data)
+int	check_flood(t_map *map, t_data *data)
 {
-	int x;
-	int y;
-	int i;
-	char **cpy_grid;
+	int		x;
+	int		y;
+	int		i;
+	char	**cpy_grid;
+
 	i = 0;
 	x = -1;
 	y = -1;
 	(void)data;
-	find_player_position(map,&x,&y);
+	find_player_position(map, &x, &y);
 	cpy_grid = cpy(map);
 	flood_fill(map, cpy_grid, y, x);
-	check_collectible_flood(map,cpy_grid,data);
-	free_cpy(cpy_grid,map->height);
+	check_collectible_flood(map, cpy_grid, data);
+	free_cpy(cpy_grid, map->height);
 	return (0);
 }
-
-
-
--- so_long/so_long.c
+++ so_long/so_long.c
@@ -1,168 +1,209 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include "minilibx-linux/mlx.h"
-#include <unistd.h>
+#include "so_long.h"
 #include <X11/X.h>
 #include <X11/keysym.h>
-#include "so_long.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int destroy_all(t_data *data)
+int	destroy_all(t_data *data)
 {
-    mlx_destroy_image(data->mlx_ptr,data->image_ptr_mur);
-    mlx_destroy_image(data->mlx_ptr,data->image_ptr_exit);
-    mlx_destroy_image(data->mlx_ptr,data->image_ptr_collectible);
-    mlx_destroy_image(data->mlx_ptr,data->image_ptr_end);
-    mlx_destroy_image(data->mlx_ptr,data->image_ptr_perso);
-    mlx_destroy_image(data->mlx_ptr,data->image_ptr_floor);
-    mlx_destroy_display(data->mlx_ptr);
-    return (0);
+	mlx_destroy_image(data->mlx_ptr, data->image_ptr_mur);
+	mlx_destroy_image(data->mlx_ptr, data->image_ptr_exit);
+	mlx_destroy_image(data->mlx_ptr, data->image_ptr_collectible);
+	mlx_destroy_image(data->mlx_ptr, data->image_ptr_end);
+	mlx_destroy_image(data->mlx_ptr, data->image_ptr_perso);
+	mlx_destroy_image(data->mlx_ptr, data->image_ptr_floor);
+	mlx_destroy_display(data->mlx_ptr);
+	return (0);
 }
 
-void    find_player_position(t_map *map, int *player_x, int *player_y)
+void	find_player_position(t_map *map, int *player_x, int *player_y)
 {
-    int i;
-    int j;
-    i = 0;
-    while(i < map->height)
-    {
-        j = 0;
-        while(j < map->width)
-        {
-            if(map->grid[i][j] == 'P')
-            {
-                *player_x = j;
-                *player_y = i;
-                return;
-            }
-            j++;
-        }
-        i++;
-    }
-   
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < map->height)
+	{
+		j = 0;
+		while (j < map->width)
+		{
+			if (map->grid[i][j] == 'P')
+			{
+				*player_x = j;
+				*player_y = i;
+				return ;
+			}
+			j++;
+		}
+		i++;
+	}
 }
 
-int move_player(t_map *map,int new_x,int new_y,int old_x,int old_y)
+int	move_player(t_map *map, int new_x, int new_y, int old_x, int old_y)
 {
-    int  i;
-    i = 0;
-    if(map->grid[new_y][new_x] == '0')
-    {
-        map->grid[old_y][old_x] = '0';
-        map->grid[new_y][new_x] = 'P';
-        return (0);
-    }
-    else if(map->grid[new_y][new_x] == 'C')
-    {
-        map->grid[old_y][old_x] = '0';
-        map->grid[new_y][new_x] = 'P';
-        i++;
-        return (0);
-    }
-    else if(map->grid[new_y][new_x] == 'E' && check_collectible(map) == -1)
-    {
-        map->grid[old_y][old_x] = '0';
-        map->grid[new_y][new_x] = 'P';
-        return (1);
-    }
-    return (0);
+	int	i;
 
+	i = 0;
+	if (map->grid[new_y][new_x] == '0')
+	{
+		map->grid[old_y][old_x] = '0';
+		map->grid[new_y][new_x] = 'P';
+		return (0);
+	}
+	else if (map->grid[new_y][new_x] == 'C')
+	{
+		map->grid[old_y][old_x] = '0';
+		map->grid[new_y][new_x] = 'P';
+		i++;
+		return (0);
+	}
+	else if (map->grid[new_y][new_x] == 'E' && check_collectible(map) == -1)
+	{
+		map->grid[old_y][old_x] = '0';
+		map->grid[new_y][new_x] = 'P';
+		return (1);
+	}
+	return (0);
 }
 
-int on_keypress(int keysym, t_data *data)
+int	on_keypress(int keysym, t_data *data)
 {
-    int player_x = -1;
-    int player_y = -1;
-    int new_x;
-    int new_y;
-    int end_or_not;
-    end_or_not = 0;
-    find_player_position(data->map, &player_x, &player_y);
-    new_x = player_x;
-    new_y = player_y;
+	int	player_x;
+	int	player_y;
+	int	new_x;
+	int	new_y;
+	int	end_or_not;
 
-    if(keysym == 97) //fleche de gauche
-        new_x -= 1;
-    else if(keysym == 119) // fleche haut
-        new_y -= 1;
-    else if(keysym == 100) // fleche droite
-        new_x += 1;
-    else if(keysym == 115) //fleche bas
-        new_y += 1;
-    else if(keysym == 65307)
-        exit_prog(data);
-    if(new_x >= 0 && new_x < data->map->width && new_y >= 0 && new_y < data->map->width)
-        end_or_not = move_player(data->map, new_x, new_y, player_x, player_y);
-    if(end_or_not == 0)
-        draw_map(data->map,data);
-    else
-        exit_prog(data);
-    printf("Presse une touche: %d\n",keysym);
-    return (0);
+	player_x = -1;
+	player_y = -1;
+	end_or_not = 0;
+	find_player_position(data->map, &player_x, &player_y);
+	new_x = player_x;
+	new_y = player_y;
+	if (keysym == 97) // fleche de gauche
+		new_x -= 1;
+	else if (keysym == 119) // fleche haut
+		new_y -= 1;
+	else if (keysym == 100) // fleche droite
+		new_x += 1;
+	else if (keysym == 115) // fleche bas
+		new_y += 1;
+	else if (keysym == 65307)
+		exit_prog(data);
+	if (new_x >= 0 && new_x < data->map->width && new_y >= 0
+		&& new_y < data->map->width)
+		end_or_not = move_player(data->map, new_x, new_y, player_x, player_y);
+	if (end_or_not == 0)
+		draw_map(data->map, data);
+	else
+		exit_prog(data);
+	printf("Presse une touche: %d\n", keysym);
+	return (0);
 }
 
-int ft_strlen_nl(char *str)
+int	ft_strlen_nl(char *str)
 {
-    int i;
-    i = 0;
-    while(str[i] != '\0' && str[i] != '\n')
-        i++;
-    return (i);
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0' && str[i] != '\n')
+		i++;
+	return (i);
 }
 
-void    malloc_grid(t_map *map, char *filename)
+int	malloc_grid(t_map *map, char *filename)
 {
-    int fd;
-    int i;
-    char *line;
-    i = 0;
-    fd = open(filename,O_RDONLY);
-    if(fd < 0)
-    {
-        perror("Error open fd\n");
-        exit(1);
-    }
-    
-    while(1)
-    {
-        line = get_next_line(fd);
-        if(!line)
-            break;
-        if(i == 0)
-            map->width = ft_strlen_nl(line);
-        i++;
-        free(line);
-    }
-    map->grid = malloc(sizeof(char *) * (i + 1));
-    map->grid[i] = NULL;
-    close (fd);
+	int		fd;
+	int		i;
+	char	*line;
+
+	i = 0;
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		perror("Error open fd\n");
+		return (-1);
+	}
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			break ;
+		if (i == 0)
+			map->width = ft_strlen_nl(line);
+		i++;
+		free(line);
+	}
+	map->grid = malloc(sizeof(char *) * (i + 1));
+	map->grid[i] = NULL;
+	close(fd);
+	return (0);
+}
+void	file_image(t_data *data, t_map *map)
+{
+	data->image_ptr_perso = mlx_xpm_file_to_image(data->mlx_ptr,
+			"image/player.xpm", &map->width, &map->height);
+	data->image_ptr_floor = mlx_xpm_file_to_image(data->mlx_ptr,
+			"image/sol.xpm", &map->width, &map->height);
+	data->image_ptr_mur = mlx_xpm_file_to_image(data->mlx_ptr, "image/wall.xpm",
+			&map->height, &map->width);
+	data->image_ptr_exit = mlx_xpm_file_to_image(data->mlx_ptr,
+			"image/zaap.xpm", &map->width, &map->height);
+	data->image_ptr_collectible = mlx_xpm_file_to_image(data->mlx_ptr,
+			"image/collecte.xpm", &map->width, &map->height);
+	data->image_ptr_end = mlx_xpm_file_to_image(data->mlx_ptr, "image/end.xpm",
+			&map->width, &map->height);
 }
 
-int main(void)
+int	check_argv(char *str)
 {
+	int	i;
 
-    t_data data;
-    t_map map;
-    data.map = &map;
-    data.mlx_ptr = mlx_init();
-    if(!data.mlx_ptr)
-        return (1);
-    data.window_ptr = mlx_new_window(data.mlx_ptr, 2000, 1000 ,"so_long");
-    if(!data.window_ptr)
-        return(free(data.mlx_ptr),1);
-    mlx_hook(data.window_ptr, DestroyNotify, StructureNotifyMask, &destroy_all, &data);
-    data.image_ptr_perso = mlx_xpm_file_to_image(data.mlx_ptr,"image/player.xpm", &map.width ,&map.height);
-    data.image_ptr_floor = mlx_xpm_file_to_image(data.mlx_ptr,"image/sol.xpm", &map.width,&map.height);
-    data.image_ptr_mur = mlx_xpm_file_to_image(data.mlx_ptr,"image/wall.xpm", &map.height, &map.width); 
-    data.image_ptr_exit = mlx_xpm_file_to_image(data.mlx_ptr,"image/zaap.xpm", &map.width, &map.height);
-    data.image_ptr_collectible = mlx_xpm_file_to_image(data.mlx_ptr,"image/collecte.xpm",&map.width,&map.height);
-    data.image_ptr_end = mlx_xpm_file_to_image(data.mlx_ptr,"image/end.xpm",&map.width,&map.height);
-    malloc_grid(&map,"map/map.ber");
-    read_map("map/map.ber",&map,&data);
-    draw_map(&map, &data);
-    mlx_hook(data.window_ptr, KeyPress, KeyPressMask, &on_keypress, &data);
-    mlx_hook(data.window_ptr,17,0,&close_window,&data);
-    mlx_loop(data.mlx_ptr);
-    return (0);
+	i = 0;
+	while (str[i])
+		i++;
+	i--;
+	if (str[i] == 'r' && str[i - 1] == 'e' && str[i - 2] == 'b' && str[i
+		- 3] == '.')
+		return (1);
+	else
+	{
+		write(2, "ERROR ARGV <map/map.ber>\n", 25);
+		exit(0);
+	}
 }
+int	main(int argc, char **argv)
+{
+	t_data	data;
+	t_map	map;
 
+	if (argc != 2)
+		exit(1);
+	data.map = &map;
+	check_argv(argv[1]);
+	data.mlx_ptr = mlx_init();
+	if (!data.mlx_ptr)
+		return (1);
+	data.window_ptr = mlx_new_window(data.mlx_ptr, 2000, 1000, "so_long");
+	if (!data.window_ptr)
+		return (free(data.mlx_ptr), 1);
+	mlx_hook(data.window_ptr, DestroyNotify, StructureNotifyMask, &destroy_all,
+		&data);
+	file_image(&data, &map);
+	if (malloc_grid(&map, argv[1]) == -1)
+	{
+		mlx_destroy_window(data.mlx_ptr, data.window_ptr);
+		destroy_all(&data);
+		free(data.mlx_ptr);
+		return (1);
+	}
+	read_map(argv[1], &map, &data);
+	draw_map(&map, &data);
+	mlx_hook(data.window_ptr, KeyPress, KeyPressMask, &on_keypress, &data);
+	mlx_hook(data.window_ptr, 17, 0, &close_window, &data);
+	mlx_loop(data.mlx_ptr);
+	return (0);
+}
