9ce00e7074e0aca2f2b7cf17daa2f3f29aa7a87f
Enzo Giuseppin <engiusep@z3r11p1.42lyon.fr>
2025-02-26 12:18:16 +0100

    flood ok

--- so_long/flood_fill.c
+++ so_long/flood_fill.c
@@ -6,7 +6,7 @@
 /*   By: engiusep <engiusep@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 15:18:34 by engiusep          #+#    #+#             */
-/*   Updated: 2025/02/18 14:00:20 by engiusep         ###   ########.fr       */
+/*   Updated: 2025/02/26 12:15:31 by engiusep         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,21 @@ int	flood_fill(t_map *map, char **grid, int y, int x)
 	flood_fill(map, grid, y, x - 1);
 	return (0);
 }
+int	flood_fill_2(t_map *map, char **grid, int y, int x)
+{
+	if (x < 0 || x >= map->width || y < 0 || y >= map->height)
+		return (-1);
+	if (grid[y][x] == 'F' || (grid[y][x] != 'P' && grid[y][x] != '0'
+			&& grid[y][x] != 'C' && grid[y][x] != 'E'))
+		return (1);
+	grid[y][x] = 'F';
+	flood_fill(map, grid, y + 1, x);
+	flood_fill(map, grid, y, x + 1);
+	flood_fill(map, grid, y - 1, x);
+	flood_fill(map, grid, y, x - 1);
+	return (0);
+}
+
 char	*cpy_string(t_map *map, char *str)
 {
 	int		i;
@@ -63,7 +78,7 @@ char	**cpy(t_map *map)
 	cpy[i] = NULL;
 	return (cpy);
 }
-int	check_collectible_flood(t_map *map, char **grid, t_data *data)
+int	check_collectible_flood(t_map *map, char **grid, t_data *data,char **grid2)
 {
 	int	i;
 	int	j;
@@ -75,13 +90,41 @@ int	check_collectible_flood(t_map *map, char **grid, t_data *data)
 		while (j < map->width)
 		{
 			if (grid[i][j] == 'F' || grid[i][j] == '1')
+			{
+				j++;
+			}
+			else
+			{
+				write(2, "erorr\n", 6);
+				free_cpy(grid, map->height);
+				free_cpy(grid2, map->height);
+				exit_prog(data);
+				exit(1);
+			}
+		}
+		i++;
+	}
+	return (0);
+}
+int	check_collectible_flood_2(t_map *map, char **grid, t_data *data)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < map->height)
+	{
+		j = 0;
+		while (j < map->width)
+		{
+			if (grid[i][j] == 'F' || grid[i][j] == '1' || grid[i][j] == 'E' || grid[i][j] == '0')
 				j++;
 			else
 			{
-				write(2, "ERRORR MAP\n", 12);
+				write(2, "stop\n", 5);
 				free_cpy(grid, map->height);
 				exit_prog(data);
-				exit(0);
+				exit(1);
 			}
 		}
 		i++;
@@ -94,6 +137,7 @@ int	check_flood(t_map *map, t_data *data)
 	int		y;
 	int		i;
 	char	**cpy_grid;
+	char	**cpy_grid2;
 
 	i = 0;
 	x = -1;
@@ -101,8 +145,12 @@ int	check_flood(t_map *map, t_data *data)
 	(void)data;
 	find_player_position(map, &x, &y);
 	cpy_grid = cpy(map);
+	cpy_grid2 = cpy(map);
+	flood_fill_2(map, cpy_grid2, y, x);
+	check_collectible_flood_2(map, cpy_grid2, data);
 	flood_fill(map, cpy_grid, y, x);
-	check_collectible_flood(map, cpy_grid, data);
+	check_collectible_flood(map, cpy_grid, data,cpy_grid2);
 	free_cpy(cpy_grid, map->height);
+	free_cpy(cpy_grid2, map->height);
 	return (0);
 }
