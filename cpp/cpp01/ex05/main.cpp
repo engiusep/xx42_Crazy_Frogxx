87fa1d5c5820fe895de54171a909f8e6dbff8892
Enzo Giuseppin <engiusep@z4r5p2.42lyon.fr>
2025-09-24 13:56:42 +0200

    cpp01 finish whihout bonus

---
+++ cpp/cpp01/ex05/main.cpp
@@ -0,0 +1,15 @@
+#include "Harl.hpp"
+
+
+
+
+int main()
+{
+    Harl test;
+
+    test.complain("INFO");
+    test.complain("DEBUG");
+    test.complain("ERROR");
+    test.complain("WARNING");
+    return 0;
+}
