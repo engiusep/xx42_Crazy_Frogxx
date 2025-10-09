0e068a7c46d6e1a1dbb933f2e8a6878fb0c8af68
engiusep <enzo74giuseppin@gmail.com>
2025-10-09 09:25:45 +0200

    cpp04 fini

---
+++ cpp/cpp04/ex02/main.cpp
@@ -0,0 +1,18 @@
+#include "Animal.hpp"
+#include "Brain.hpp"
+
+
+int main()
+{
+    //Animal  *animal = new Animal();
+    Animal *cat = new Cat();
+    Animal *dog = new Dog();
+
+    dog->setIdea(99,"chein manger");
+    cat->setIdea(12,"dormir");
+    //cat->getIdea(12);
+    cat->print();
+    dog->print();
+    delete cat;
+    delete dog;
+}
